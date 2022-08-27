fb025080f9323813bd23e877fbd3e58c22cb3a5d
JairIzaguirre <izaguirre.dlf@gmail.com>
2022-08-27 16:15:02 +0200

    Cambio de Imagenes y textos de apartado Servicios

--- src/app/servicios-dentales/servicios-dentales.component.ts
+++ src/app/servicios-dentales/servicios-dentales.component.ts
@@ -12,38 +12,38 @@ export class ServiciosDentalesComponent implements OnInit {
   servicios:DialogData[] = [
     {
       header:'Estética Dental',
-      shortDescription:'La especialidad abarca una gran variedad de procedimientos. El cirujano maxilofacial realiza desde complejas intervenciones de cirugía plástica hasta extracciones de muelas del juicio, apicectomías o colocación de implantes dentales.',
-      imgUrl:'assets/images/instagram/Imagen_Servicios_Estética-Dental.jpg'
+      shortDescription:'Por que la belleza de una sonrisa es tu carta de presentación, en sonreir manejamos tratamientos de alta estética dental, como Resinas, Carillas, Diseño de Sonrisa, Coronas libres de metal. Nuestros especialistas certificados te darán una experiencia como nunca.',
+      imgUrl:'assets/images/instagram/Imagen_Servicio_Estetica_Dental.jpeg'
     },
     {
-      header:'Implantes',
-      shortDescription:'La implantología tiene como objetico sustituir dientes perdidos mediante la colocación quirúrgica De un implante dental en el hueso maxilar o mandibular. El implante dental es un tornillo de titanio que se inserta en el hueso de la mandíbula o del maxilar para sustituir el diente que falta como si de un diente natural se tratara. El éxito de un implante radica en un buen diagnostico por parte de los especialistas involucrados, y en Sonreír Dental Center los más calificados tenemos para ti.',
-      imgUrl:'assets/images/instagram/Imagen_Servicios_Implantes.jpg'
+      header:'Implantes Dentales',
+      shortDescription:'La implantología tiene como objetivo sustituir dientes perdidos mediante la colocación quirúrgica de un implante dental en el hueso maxilar o mandibular. El implante dental es un tornillo de titanio que se inserta en el hueso de la mandíbula o del maxilar para sustituir el diente que falta como si de un diente natural se tratara. El éxito de un implante radica en un buen diagnostico por parte de los especialistas involucrados, y en Sonreír Dental Center los más calificados tenemos para ti.',
+      imgUrl:'assets/images/instagram/Imagen_Implantes.jpeg'
     },
     {
       header:'Odontopediatría',
-      shortDescription:'Los problemas bucales de los bebes y de los niños son tratados por el área de la odontopediatría. Esta rama está especializada en dar solución a las necesidades que los más pequeños puedan tener en relación con la salud de su boca. También en Sonreír Dental Center contamos con las odontopediatrías más dedicadas y conscientes de la responsabilidad de atender a nuestros peques.',
-      imgUrl:'assets/images/instagram/Imagen_Servicios_Odontopediatría.jpg'
+      shortDescription:'Los problemas bucales de los bebes y de los niños son tratados por el área de la odontopediatría.  Es muy importante que el primer contacto sea con un profesional experimentado en tratar a los mas pequeños.En Sonreír Dental Center contamos con l@s odontopediatrías más dedicadas y conscientes de la responsabilidad de atender a nuestros peques.',
+      imgUrl:'assets/images/instagram/Imagen_Odontopediatria.jpeg'
     },
     {
-      header:'Rehabilitación protéstica',
-      shortDescription:'Si devolver la función estetica y la armonia oral es lo que requerimos, en Sonreir Dental Center nuestros clientes especialistas mediante la protesis dentales solucionan las perdidas de dientes, siempre buscando una oclusióm y función estética.',
-      imgUrl:'assets/images/instagram/Imagen_Servicios_Rehabilitación-Protéstica.jpg'
+      header:'Rehabilitación Integral',
+      shortDescription:'Con la tecnología de punta podemos devolver la función estetica y la armonia oral a cualquier paciente, en Sonreir Dental Center nuestros especialistas mediante la protesis dentales solucionan las perdidas de dientes, siempre buscando una oclusión y función estética.',
+      imgUrl:'assets/images/instagram/Imagen_Servicio_Rehabilitacion.jpeg'
     },
     {
       header:'Ortodoncia',
-      shortDescription:'Nuestros especialistas son miembros de las sociedades científicas nacionales e internacionales mas importantes en el ámbito de la ortodoncia, asimismo cuentan con certificación en los sistemas mas avanzados tales como damon, Invisalign, CCO, entre otros.',
-      imgUrl:'assets/images/instagram/Imagen_Servicios_Ortodoncía.jpg'
+      shortDescription:'Especialidad encargada de corregir la posición de los dientes y mejorar función y estética. Nuestros especialistas son miembros de las sociedades científicas nacionales e internacionales mas importantes en el ámbito de la ortodoncia, asimismo cuentan con certificación en los sistemas mas avanzados tales como damon, Invisalign, CCO, entre otros.',
+      imgUrl:'assets/images/instagram/Imagen_Ortodoncia.jpeg'
     },
     {
       header:'Endodoncia',
-      shortDescription:'Valoramos tus dientes! Y en nuestro principio conservador tenemos a la endodoncia como una gran aliada. La Endodoncia (o tratamiento de Conducto) es un procedimiento que consiste en reparar y salvar un diente gravemente dañado o infectado en vez de extraerlo. En Sonreír Dental Center tenemos los mejores especialistas en endodoncia que te garantizan una solucion definitiva, efectiva y garantizada.',
-      imgUrl:'assets/images/instagram/Imagen_Servicios_Endodoncia.jpg'
+      shortDescription:'La Endodoncia (o tratamiento de Conducto) es un procedimiento que consiste en reparar y salvar un diente gravemente dañado o infectado en vez de extraerlo. En Sonreír Dental Center tenemos los mejores especialistas en endodoncia que te garantizan una solucion definitiva, efectiva y garantizada.',
+      imgUrl:'assets/images/instagram/Imagen_Endodoncia.jpeg'
     },
     {
-      header:'Cirugía Maxilofacial',
-      shortDescription:'La especialidad abarca una gran variedad de procedimientos. El cirujano maxilofacial realiza desde complejas intervenciones de cirugía plástica hasta extracciones de mueleas del juicio, apicectomias o colocación de implantes dentales',
-      imgUrl:'assets/images/instagram/Imagen_Servicios_Cirugía-Maxilofacial.jpg'
+      header:'Cirugía Oral',
+      shortDescription:'La especialidad abarca una gran variedad de procedimientos. El cirujano maxilofacial realiza desde complejas intervenciones de cirugía plástica hasta extracciones de muelas del juicio, apicectomias o biopsias intraorales.',
+      imgUrl:'assets/images/instagram/Imagen_Cirugia_Maxilofacial.jpeg'
     },
   ]
   arr: number[] = [1, 2, 3];
